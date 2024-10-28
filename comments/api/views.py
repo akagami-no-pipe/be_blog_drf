bd7a0268ce2f1dc8a4be7c38d5696cc004f3d033
akagami-no-pipe <fhernandezc9@gmail.com>
2024-10-27 23:40:16 -0300

    Se agrega ordenamiento y permisos a APIs de comments

--- comments/api/views.py
+++ comments/api/views.py
@@ -1,10 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from comments.models import Comment
 from comments.api.serializers import CommentSerializer
+from comments.api.permissions import IsOwnerOrReadAndCreateOnly
 
 class CommentApiViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadAndCreateOnly]
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    ordering = ['-created_at']
     filterset_fields = ['post']
